398d08795bf390492e220571d38ee4a58a54e951
Bobb-93 <borisiakimov@gmail.com>
2025-02-26 18:35:06 +0200

    created dom object with resetButton as property

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-// When user click on reset button
-function clearVal() {
+const dom ={
+    resetButton: document.getElementById("reset-button")
+};
+
+dom.resetButton.addEventListener("click", () =>{
     window.location.reload();
     document.getElementsByClassName("finalValue").innerHTML = "";
-};
+    
+});
