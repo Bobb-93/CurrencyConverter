3be2d858035f7016625dd9b4d1c3b1e57c628db9
Bobb-93 <borisiakimov@gmail.com>
2025-02-26 18:42:40 +0200

    add getElementById for various elements

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const dom ={
-    resetButton: document.getElementById("reset-button")
+    enterAmount: document.getElementById("enter-amount"),
+    currencyAmount: document.getElementById("currency-amount"),
+    selectFrom: document.getElementById("select-from"),
+    toCurrency: document.getElementById("to-currency"),
+    selectTo: document.getElementById("select-to"),
+    convertButton: document.getElementById("convert-button"),
+    resetButton: document.getElementById("reset-button"),
+    finalValue: document.getElementById("final-value"),
 };
+
+let url = "https://v6.exchangerate-api.com/v6/6880e928c542f12ec6425196/latest/USD";
 
 dom.resetButton.addEventListener("click", () =>{
     window.location.reload();
-    document.getElementsByClassName("finalValue").innerHTML = "";
+    dom.finalValue.innerHTML = "";
     
 });
